0abdd03cb7377848459c723ef0409a0c5071203d
kiseop91 <kiseop91@naver.com>
2020-07-04 17:16:14 +0900

    UI 셋팅 ( 메뉴바 에니메이션 적용 )

--- PomordorUI/mainwindow.cpp
+++ PomordorUI/mainwindow.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+#include <QPropertyAnimation>
+
 static int value = 0;
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
-
+	ui->setupUi(this);
+	
+	//setWindowFlags(Qt::Window | Qt::WindowStaysOnTopHint |  Qt::FramelessWindowHint);
 	QObject::connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(onClickPushButton()));
+	QObject::connect(ui->Btn_Toggle, SIGNAL(clicked()), this, SLOT(onClickToggleButton()));
 }
 
 void MainWindow::onClickPushButton()
@@ -17,6 +22,30 @@ void MainWindow::onClickPushButton()
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
+}
+
+void MainWindow::onClickToggleButton()
+{
+	//DEFAULT SETTING VALUES
+	size_t width = ui->frame_left_menu->width();
+	size_t height = ui->frame_left_menu->height();
+	size_t maxExtend = 250;
+	size_t standard = 70;
+	size_t widthExtended = 0;
+
+	if (width == 70)
+		widthExtended = maxExtend;
+	else
+		widthExtended = standard;
+
+	//ANIMATION
+	QPropertyAnimation* animation = new QPropertyAnimation(ui->frame_left_menu, "size");
+	animation->setDuration(400);
+	animation->setStartValue(QSize(width, height));
+	animation->setEndValue(QSize(widthExtended, height));
+	animation->setEasingCurve(QEasingCurve::InOutSine);
+	animation->start();
+
 }
 
