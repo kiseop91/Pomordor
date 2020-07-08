3cdf8de13ce44b786f217c23b25fd67564240eab
zxwnstn <45479885+zxwnstn@users.noreply.github.com>
2020-07-08 23:47:22 +0900

    엔진과 UI 연동 (#17)
    CMakeLists수정 및 간단한 함수로 확인

---
+++ PomordorEngine/Util.cpp
@@ -0,0 +1,11 @@
+#include "pegpch.h"
+#include "Util.h"
+
+namespace Pome{
+
+    void printHello()
+    {
+        std::cout << "hello world" << std::endl;
+        return;
+    }
+}
--- PomordorUI/pompch.h
+++ PomordorUI/pompch.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
+//Engine
+#include <Pome.h>
 
 //qt
 #include <QMainWindow>
