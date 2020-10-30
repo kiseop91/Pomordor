738b47afb3ffe3d308c3bda87dd57825b8076749
joseph <joseph@clo3d.com>
2020-10-30 15:10:56 +0900

    4K Support

--- PomordorUI/src/main.cpp
+++ PomordorUI/src/main.cpp
@@ -1,9 +1,15 @@
 #include "pompch.h"
 #include "Timer/Timer.h"
+#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+	SetProcessDPIAware();
+
     QApplication a(argc, argv);
+				
 	QFontDatabase::addApplicationFont(":/font/fonts/segoeui.ttf");
 	QFontDatabase::addApplicationFont(":/font/fonts/D2Coding.ttf");
 
