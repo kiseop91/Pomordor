15b94091570bab4b11a6d82e294f768771675b40
joseph <joseph@clo3d.com>
2020-10-30 14:53:37 +0900

    UI Refactoring

--- PomordorUI/src/main.cpp
+++ PomordorUI/src/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 	QFontDatabase::addApplicationFont(":/font/fonts/segoeui.ttf");
+	QFontDatabase::addApplicationFont(":/font/fonts/D2Coding.ttf");
 
     Timer t;
     t.show();
