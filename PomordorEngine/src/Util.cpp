b00a09da11f1e4f0fc3acd62bc12c966bd7584d4
zxwnstn2 <60541888+zxwnstn2@users.noreply.github.com>
2020-11-01 20:21:29 +0900

    Refactor/submodule (#25)
    * Add json library and move directory of googletest
    
    * fix Cmake Error
    
    Co-authored-by: zxwnstn <zxwnstn@google.com>

--- PomordorEngine/src/Util.cpp
+++ PomordorEngine/src/Util.cpp
@@ -8,4 +8,13 @@ namespace Pome{
         std::cout << "hello world" << std::endl;
         return;
     }
+	void printJson()
+	{
+		Json::Value root;
+		root["hello"] = "jsoncpp";
+		
+		std::ofstream out("hello.json", std::ios::out);
+		out << root;
+		out.close();
+	}
 }
