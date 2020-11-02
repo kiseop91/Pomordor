4f47b91badc4a2d547ec527c98cb980a7410d3ee
zxwnstn <45479885+zxwnstn@users.noreply.github.com>
2020-11-02 16:04:10 +0900

    Refactor/post build (#27)
    * Refactoring cmakes...
    
    * delete zxwnstn's jsoncpp submodule
    
    * add original jsoncpp submodule
    
    * Add qt designer
    
    * fix qt5 moc warning
    
    Co-authored-by: zxwnstn <zxwnstn@google.com>
    Co-authored-by: kiseop kim <kiseop91@naver.com>

---
+++ cmake/googletest_config.cmake
@@ -0,0 +1,4 @@
+
+option(BUILD_GMOCK OFF)
+
+set(GTestProj gtest gtest_main)
---
+++ cmake/pomordor_config.cmake
@@ -0,0 +1,5 @@
+
+OPTION(BUILD_PE_LIB "Build Pomordor engine as Dll" OFF)
+OPTION(BUILD_PE_STATIC "Build Pomordor engine as LIB" ON)
+
+set(PomordorProj PomordorEngine PomordorUI ExampleTests)
